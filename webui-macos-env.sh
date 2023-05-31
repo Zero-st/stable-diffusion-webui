500019a67975874efac90a1217a2e426679f1590
Zero-st <31891863+Zero-st@users.noreply.github.com>
2023-05-31 23:04:29 +0800

    Update webui-macos-env.sh

--- webui-macos-env.sh
+++ webui-macos-env.sh
@@ -9,6 +9,7 @@ then
     python_cmd="python3.10"
 fi
 
+
 export install_dir="$HOME"
 export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
 export TORCH_COMMAND="pip install torch==2.0.1 torchvision==0.15.2"
